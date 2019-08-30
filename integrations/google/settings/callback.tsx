14557abb112668939f0a816fd123a8fc33f51ccb
James Macfie <james@macfie.co.nz>
2019-08-30 20:25:48 +1200

    Move callback

--- integrations/google/settings/callback.tsx
+++ integrations/google/settings/callback.tsx
@@ -1,5 +1,44 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
+import { observer } from 'mobx-react-lite';
+import PageWrapper from 'containers/pageWrapper';
+import IntegrationsStore, { StoreContext } from 'stores/integrations';
 
-const Callback = () => <p>callback</p>;
+const Inner = observer(() => {
+  const isServer = typeof window === 'undefined';
+  if (isServer) {
+    return null;
+  }
 
-export default Callback;
+  const [saved, setSaved] = useState<boolean | null>(null);
+  const store = useContext(StoreContext) as IntegrationsStore;
+  const { query } = useRouter();
+
+  useEffect(() => {
+    store
+      .saveNewIntegration('google', {
+        code: query.code
+      })
+      .then(setSaved);
+  }, [null]);
+
+  if (saved === null) {
+    return <p>Saving</p>;
+  }
+
+  if (saved === false) {
+    return <p>Something went wrong</p>;
+  }
+
+  return <p>Saved! hoorah!</p>;
+});
+
+const GoogleCallback = () => {
+  return (
+    <PageWrapper title="">
+      <Inner />
+    </PageWrapper>
+  );
+};
+
+export default GoogleCallback;
--- integrations/google/settings/something/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const Something = () => <p>something</p>;
-
-export default Something;
