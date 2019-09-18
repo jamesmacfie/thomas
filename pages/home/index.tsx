cc8fd102f1d9c69e69f10b234e087a774c917f26
James Macfie <james@macfie.co.nz>
2019-09-18 18:13:30 +1200

    Missing rename

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react';
 import Router from 'next/router';
 import { observer } from 'mobx-react-lite';
-import PageWrapper from 'containers/wrappers/pageWrapper';
+import PageWrapper from 'containers/wrappers/page';
 import { StoreContext as DeviceViewsStoreContext } from 'stores/deviceViews';
 
 const Inner = observer(() => {
