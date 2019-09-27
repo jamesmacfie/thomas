b9051336d3063f94c1b083421e34a914845e9e9e
James Macfie <james@macfie.co.nz>
2019-09-27 19:51:59 +1200

    Scroll component `scrollTo` reset

--- components/scroll/index.tsx
+++ components/scroll/index.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from 'react';
 import cn from 'classnames';
+import { useDebouncedCallback } from 'use-debounce';
 import './styles.css';
 
 interface Props {
@@ -8,11 +9,21 @@ interface Props {
 }
 
 const Scroll = ({ children, className }: Props) => {
+  const [debouncedOnScroll] = useDebouncedCallback((target: any) => {
+    console.log(target);
+    target.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth'
+    });
+  }, 10000);
   return (
     <div className={cn('relative', className)}>
-      <div className="absolute left-0 right-0 top-0 h-4 z-10 scroll-top" />
-      <div className="h-full overflow-y-scroll">{children}</div>
-      <div className="absolute left-0 right-0 bottom-0 z-10 h-4 scroll-bottom" />
+      <div className="absolute left-0 right-0 top-0 h-4 scroll-top" />
+      <div onScroll={e => debouncedOnScroll(e.target)} className="h-full overflow-y-scroll">
+        {children}
+      </div>
+      <div className="absolute left-0 right-0 bottom-0 h-4 scroll-bottom" />
     </div>
   );
 };
