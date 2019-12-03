cbd48d7f293a8f180393f64f728726bd799f5839
James Macfie <james@macfie.co.nz>
2019-12-03 21:35:01 +1300

    Default formik Map to Te Papa

--- components/formikInput/map.tsx
+++ components/formikInput/map.tsx
@@ -2,6 +2,9 @@ import React from 'react';
 import { LeafletMouseEvent } from 'leaflet';
 import Map from 'components/map';
 
+// Te Papa, Wellington
+const defaultValue = [-41.2903355647969, 174.78192731738093];
+
 interface Props {
   form: any;
   name: string;
@@ -12,12 +15,11 @@ const FormikInputMap = ({ form, name, value, ...props }: Props) => {
   console.log('value', value);
   let valueToUse;
   if (!value || !value[0] || !value[1]) {
-    valueToUse = [-41.2903355647969, 174.78192731738093];
+    valueToUse = defaultValue;
   } else {
     valueToUse = value;
   }
   const onClick = (e: LeafletMouseEvent) => {
-    console.log(e);
     form.setFieldValue(name, [e.latlng.lat, e.latlng.lng, e.latlng.alt]);
   };
   return <Map onClick={onClick} lat={valueToUse[0]} lng={valueToUse[1]} className="mb-4" {...props} />;
